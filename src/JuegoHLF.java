2a401f2b6690bcd4f0f9c5df3ab97db745400a8b
MariaMateos <mateostorner@gmail.com>
2022-12-09 10:08:35 +0100

    P30

---
+++ src/JuegoHLF.java
@@ -0,0 +1,10 @@
+
+public class JuegoHLF {
+	
+	public static void main(String[]args) {
+		Tablero t1=new Tablero(8,'*');
+		//t1.rellena('*');
+		t1.setCasilla(3, 3,' ');
+		System.out.println();
+	}
+}
---
+++ src/Tablero.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class Tablero {
+	
+	private char[][] tablero;
+	
+	public Tablero(int tamaño, char caracter) {
+		this.tablero=new char[tamaño][tamaño];
+		rellena(caracter);
+	}
+	public String toString() {
+		String res="";
+		for(int i=0; i<tablero.length;i++) {
+			for(int j=0;j<tablero[i].length;j++) {
+				res+=tablero[i][j]+" ";
+			}
+			res+="\n";
+		}
+		return res;
+	}
+	
+	public void rellena(char caracter) {
+		for(int i=0; i<tablero.length;i++) {
+			for(int j=0;j<tablero[i].length;j++) {
+				tablero[i][j]=caracter;
+			}
+		}
+	}
+	
+	public char getcasilla (int ancho,int alto) {
+		return tablero[ancho][alto];
+	}
+	public void setCasilla (int ancho, int alto, char cambio) {
+		tablero[ancho][alto];
+	}
+	
+	
+	
+	
+	
+	
+
+}
